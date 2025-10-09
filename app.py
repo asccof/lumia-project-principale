ccf648778ff4fb549eb389483b95ac8578bdfa72
asccof <hilalidriss07@gmail.com>
2025-10-09 10:31:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -244,7 +244,10 @@ def login_post():
     return redirect(next_url)
 from flask_login import logout_user, login_required
 
-@app.get("/logout")
+from flask import redirect, url_for, flash
+from flask_login import logout_user, login_required
+
+@app.get("/logout")  # endpoint implicite = 'logout'
 @login_required
 def logout():
     logout_user()
